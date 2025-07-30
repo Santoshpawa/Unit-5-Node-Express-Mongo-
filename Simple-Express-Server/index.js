bee88a1214c170efda7b8a7ac7908b7f4d2996f7
Santosh Singh Pawar <textingsantosh@gmail.com>
2025-07-30 10:52:47 +0530

    program-made

---
+++ Simple-Express-Server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/home", (req, res) => {
+  res.send("This is home page");
+});
+
+app.get("/contactus", (req, res) => {
+  res.send("Contact us at contact@support.com");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
